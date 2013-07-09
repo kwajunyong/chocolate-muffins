deec657598d6d2e995f2983d09486e82f2a7728f
Jun Yong <junyong.kwa@gmail.com>
2013-07-10 02:57:11 +0800

    Added searching & matching methods in AST

--- Project/source/AST.cpp
+++ Project/source/AST.cpp
@@ -64,3 +64,128 @@ std::string AST::indent(int level)
 
 	return indent;
 }
+
+bool AST::matchTree(ASTNode* tree, ASTNode* compare)
+{
+	if (tree == NULL && compare == NULL) {
+		return true;
+	}
+
+	if (tree == NULL || compare == NULL) {
+		return false;
+	}
+
+	if (tree->equals(compare)) {
+		ASTNode* nodeChild = tree->getChild();
+		ASTNode* compareChild = compare->getChild();
+
+		while (true) {
+			if (nodeChild != NULL && compareChild != NULL) {
+				if (matchTree(nodeChild, compareChild)) {
+					nodeChild = nodeChild->getNext();
+					compareChild = compareChild->getNext();
+
+					continue;
+				} else {
+					return false;
+				}
+			}
+
+			if (nodeChild == NULL && compareChild == NULL) {
+				return true;
+			}
+
+			if (nodeChild == NULL || compareChild == NULL) {
+				return false;
+			}
+		}
+	}
+}
+
+bool AST::matchSubTree(ASTNode* tree, ASTNode* subtree)
+{
+	if (subtree == NULL) {
+		return true;
+	}
+
+	if (tree == NULL) {
+		return false;
+	}
+
+	if (matchTree(tree, subtree)) {
+			return true;
+	}
+
+	ASTNode* child = tree->getChild();
+
+	while (child != NULL) {
+		if (matchSubTree(child, subtree)) {
+			return true;
+		} else {
+			child = child->getNext();
+		}
+	}
+
+	return false;
+}
+
+ASTNode* AST::getStatementNode(int stmtNum)
+{
+	std::vector<ASTNode*> list;
+
+	search(_root, list, stmtNum, NONE);
+
+	return list.front();
+}
+
+std::vector<ASTNode*> AST::getStatementNodes(ASTType type)
+{
+	std::vector<ASTNode*> list;
+
+	search(_root, list, 0, type);
+
+	return list;
+}
+
+std::vector<int> AST::getStatementNumbers(ASTType type)
+{
+	std::vector<int> list;
+
+	search(_root, list, type);
+
+	return list;
+}
+
+void AST::search(ASTNode* node, std::vector<ASTNode*> &list, int stmtNum, ASTType type)
+{
+	while (node != NULL) {
+		if (node->getType() != NONE && node->getType() == type) {
+			list.push_back(node);
+		}
+
+		if (node->getStatementNumber() != 0 && node->getStatementNumber() == stmtNum) {
+			list.push_back(node);
+		}
+
+		if (node->getChild() != NULL) {
+			search(node->getChild(), list, stmtNum, type);
+		}
+		
+		node = node->getNext();
+	}
+}
+
+void AST::search(ASTNode* node, std::vector<int> &list, ASTType type)
+{
+	while (node != NULL) {
+		if (node->getType() != NONE && node->getType() == type) {
+			list.push_back(node->getStatementNumber());
+		}
+
+		if (node->getChild() != NULL) {
+			search(node->getChild(), list, type);
+		}
+		
+		node = node->getNext();
+	}
+}
--- Project/source/AST.h
+++ Project/source/AST.h
@@ -16,11 +16,20 @@ public:
 
 	std::vector<std::string> traverse(ASTNode* node);
 
+	bool matchTree(ASTNode* tree, ASTNode* compare);
+	bool matchSubTree(ASTNode* tree, ASTNode* subtree);
+
+	ASTNode* getStatementNode(int stmtNum);
+	std::vector<ASTNode*> getStatementNodes(ASTType type);
+	std::vector<int> getStatementNumbers(ASTType type);
 private:
 	ASTNode* _root;
 
 	void traverse(ASTNode* node, std::vector<std::string> &list, int level);
 
 	std::string indent(int level);
+
+	void search(ASTNode* node, std::vector<ASTNode*> &list, int stmtNum, ASTType type);
+	void search(ASTNode* node, std::vector<int> &list, ASTType type);
 };
 
