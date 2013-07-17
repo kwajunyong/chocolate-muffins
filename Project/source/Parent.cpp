cc5f9bd31b9bb3b8337f52a371f6b0c0e0add398
Jun Yong <junyong.kwa@gmail.com>
2013-07-18 01:00:10 +0800

    Added Parent

---
+++ Project/source/Parent.cpp
@@ -0,0 +1,93 @@
+#include "Parent.h"
+
+Parent::Parent(void)
+{}
+
+Parent::~Parent(void)
+{}
+
+bool Parent::addParent(int stmtNum1, int stmtNum2)
+{
+	if (parentMap[stmtNum2] < 1) {
+		childMap[stmtNum1].push_back(stmtNum2);
+		parentMap[stmtNum2] = stmtNum1;
+
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Parent::isParent(int stmtNum1, int stmtNum2, bool transitive)
+{
+	if (!transitive) {
+		return parentMap[stmtNum2] == stmtNum1;
+	} else {
+		return transitiveSearch(stmtNum1, stmtNum2);
+	}
+}
+
+vector<int> Parent::getChild(int stmtNum, bool transitive)
+{
+	if (!transitive) {
+		return childMap[stmtNum];
+	} else {
+		vector<int> results;
+
+		dfs(results, stmtNum);
+
+		return results;
+	}
+}
+
+vector<int> Parent::getParent(int stmtNum, bool transitive)
+{
+	vector<int> results;
+
+	if (!transitive) {
+		int value = parentMap[stmtNum];
+
+		if (value > 0) {
+			results.push_back(value);
+		}
+	} else {
+		transitiveSearch(results, stmtNum);
+	}
+
+	return results; 
+}
+
+void Parent::dfs(vector<int> &output, int key)
+{
+	vector<int> keys = childMap[key];
+
+	for (size_t i = 0; i < keys.size(); i++) {
+		output.push_back(keys[i]);
+
+		dfs(output, keys[i]);
+	}
+}
+
+void Parent::transitiveSearch(vector<int> &output, int key)
+{
+	int value = parentMap[key];
+
+	if (value > 0) {
+		output.push_back(value);
+
+		transitiveSearch(output, value);
+	}
+}
+
+bool Parent::transitiveSearch(int key, int value)
+{
+	int search = parentMap[value];
+
+	if (search == key) {
+		return true;
+	} else if (search < 1) {
+		return false;
+	} else {
+		return transitiveSearch(key, search);
+	}
+}
---
+++ Project/source/Parent.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Parent
+{
+public:
+	Parent(void);
+	~Parent(void);
+
+	bool Parent::addParent(int stmtNum1, int stmtNum2);
+	bool isParent(int stmtNum1, int stmtNum2, bool transitive);
+	vector<int> getChild(int stmtNum, bool transitive);
+	vector<int> getParent(int stmtNum, bool transitive);
+
+private:
+	map<int, vector<int>> childMap;
+	map<int, int> parentMap;
+	
+	void dfs(vector<int> &output, int stmtNum);
+	void transitiveSearch(vector<int> &output, int key);
+	bool transitiveSearch(int key, int value);
+
+};
+
