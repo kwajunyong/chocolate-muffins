3a564becc55e881bbee244371bac8f9ce975daf7
Jun Yong <junyong.kwa@gmail.com>
2013-08-01 05:03:45 +0800

    Changed DesignExtractor
    - Added checking for recursive calls
    - Added extracting of Calls, Modifies & Uses
    - Unable to extract Next

--- Project/source/DesignExtractor.cpp
+++ Project/source/DesignExtractor.cpp
@@ -1,4 +1,4 @@
-#include "DesignExtractor.h"
+﻿#include "DesignExtractor.h"
 
 DesignExtractor::DesignExtractor(void)
 {}
@@ -11,32 +11,33 @@ void DesignExtractor::extract(PKB* pkb)
 	_pkb = pkb;
 
 	ASTNode* rootNode = _pkb->getAST()->getRootNode();
-
 	_varTable = _pkb->getVarTable();
 	_procTable = _pkb->getProcTable();
-
 	_numOfStmt = _pkb->getNumOfStmt();
 
 	_follows = new Follows();
 	_parent = new Parent();
-
 	_modifies = new Modifies(_numOfStmt, _varTable, _procTable);
 	_uses = new Uses(_numOfStmt, _varTable, _procTable);
+	_calls = new Calls();
 
 	std::vector<int> statements;
 	std::string procedure = "";
-
-	traverseAndExtract(rootNode, statements, procedure, 0);
+	traverseAST(rootNode, statements, procedure);
 
 	pkb->setFollows(_follows);
 	pkb->setParent(_parent);
 	pkb->setModifies(_modifies);
 	pkb->setUses(_uses);
+	pkb->setCalls(_calls);
+
+	traverseCalls();
+	extractFromCallNodes();
 }
 
-void DesignExtractor::traverseAndExtract(ASTNode* node, std::vector<int> statements, std::string procedure, int prevLine)
+void DesignExtractor::traverseAST(ASTNode* node, std::vector<int> statements, std::string procedure)
 {
-	while (node != NULL) {		
+	while (node != NULL) {
 		updateStatements(node, statements);
 		updateProcedure(node, procedure);
 		
@@ -49,7 +50,7 @@ void DesignExtractor::traverseAndExtract(ASTNode* node, std::vector<int> stateme
 		extractCalls(node, procedure);
 
 		if (node->getChild() != NULL) {
-			traverseAndExtract(node->getChild(), statements, procedure, prevLine);
+			traverseAST(node->getChild(), statements, procedure);
 		}
 
 		node = node->getNext();
@@ -122,52 +123,129 @@ void DesignExtractor::extractCalls(ASTNode* node, std::string procedure)
 		validateCall(node);
 			
 		//std::cout << "Calls(" << procedure << ", " << node->getName() << ")" << std::endl;
+		_calls->addCalls(procedure, node->getName());
 
-		_calls.push_back(node);
+		_callNodes.push_back(node);
 	}
 }
 
-//void DesignExtractor::extractModifiesFromCallNodes()
-//{
-//	ASTNode* node;
-//	std::vector<std::string> variables;
-//
-//	for (int i = 0; i < _calls.size(); i++) {
-//		node = _calls[i];
-//		
-//		variables = _modifies->getModifiedVar(node->getName());
-//
-//		while (isStatement(node)) {
-//			for (int j = 0; j < variables.size(); j++) {
-//				//std::cout << "Modifies(" << node->getStatementNumber() << ", " << variables[j] << ")" << std::endl;
-//				_modifies->addModifiesStmt(node->getStatementNumber(), variables[j]);
-//			}
-//
-//			node = node->getParent()->getParent();
-//		}
-//	}
-//}
-//
-//void DesignExtractor::extractUsesFromCallNodes()
-//{
-//	ASTNode* node;
-//	std::vector<std::string> variables;
-//
-//	for (int i = 0; i < _calls.size(); i++) {
-//		node = _calls[i];
-//		
-//		variables = _uses->getUsedVar(node->getName());
-//
-//		while (isStatement(node)) {
-//			for (int j = 0; j < variables.size(); j++) {
-//				//std::cout << "Uses(" << node->getStatementNumber() << ", " << variables[j] << ")" << std::endl;
-//				_uses->addUsesStmt(node->getStatementNumber(), variables[j]);
-//			}
-//
-//			node = node->getParent()->getParent();
-//		}
-//	}
-//}
+void DesignExtractor::validateCall(ASTNode* node)
+{
+	if (_procTable->getIndex(node->getName()) < 0) {
+		throw ParseException(node->getStatementNumber(), node->getName(), "Called procedure does not exist");
+	}
+}
+
+void DesignExtractor::traverseCalls()
+{
+	std::vector<std::pair<std::string, std::string>> calls;
+	std::vector<int> visited(_procTable->getSize(), 0);
+
+	for (int i = 0; i < _procTable->getSize(); i++) {
+		if (visited[i] == 0) {
+			traverseCalls(_procTable->getName(i), visited, calls);
+		}
+	}
+
+	while (!calls.empty()) {
+		//std::cout << "Calls(" << calls.back().first << ", " << calls.back().second << ")" << std::endl;
+		extractModifiesFromCall(calls.back());
+		extractUsesFromCall(calls.back());
+		
+		calls.pop_back();
+	}
+}
+
+void DesignExtractor::traverseCalls(std::string procedure, std::vector<int> &visited, std::vector<std::pair<std::string, std::string>> &calls)
+{
+	visited[_procTable->getIndex(procedure)] = 1;
+
+	std::vector<std::string> calledProc = _calls->getCalled(procedure, false);
+
+	for (int i = 0; i < calledProc.size(); i++) {
+		if (visited[_procTable->getIndex(calledProc[i])] == 0) {
+			traverseCalls(calledProc[i], visited, calls);
+		} else if (visited[_procTable->getIndex(calledProc[i])] == 1) {
+			throw ParseException("call " + procedure, "Recursive procedure call");
+		}
+	}
+
+	visited[_procTable->getIndex(procedure)] = 2;
+
+	for (int i = 0; i < calledProc.size(); i++) {
+		calls.push_back(make_pair<std::string, std::string>(calledProc[i], procedure));
+	}
+}
+
+void DesignExtractor::extractModifiesFromCall(std::pair<std::string, std::string> call)
+{
+	std::vector<std::string> variables = _modifies->getModifiedVar(call.second);
+
+	for (int i=0; i<variables.size(); i++) {
+		//std::cout << "Modifies(" << call.first << ", " << variables[i] << ")" << std::endl;
+		_modifies->addModifiesProc(call.first, variables[i]);
+	}
+}
+
+void DesignExtractor::extractUsesFromCall(std::pair<std::string, std::string> call)
+{
+	std::vector<std::string> variables = _uses->getUsedVar(call.second);
+
+	for (int i=0; i<variables.size(); i++) {
+		//std::cout << "Uses(" << call.first << ", " << variables[i] << ")" << std::endl;
+		_uses->addUsesProc(call.first, variables[i]);
+	}
+}
+
+void DesignExtractor::extractFromCallNodes()
+{
+	extractModifiesFromCallNodes();
+	extractUsesFromCallNodes();
+
+	_callNodes.clear();
+}
+
+void DesignExtractor::extractModifiesFromCallNodes()
+{
+	ASTNode* node;
+	std::vector<std::string> variables;
+
+	for (int i = 0; i < _callNodes.size(); i++) {
+		node = _callNodes[i];
+		
+		variables = _modifies->getModifiedVar(node->getName());
+
+		while (isStatement(node)) {
+			for (int j = 0; j < variables.size(); j++) {
+				//std::cout << "Modifies(" << node->getStatementNumber() << ", " << variables[j] << ")" << std::endl;
+				_modifies->addModifiesStmt(node->getStatementNumber(), variables[j]);
+			}
+
+			node = node->getParent()->getParent();
+		}
+	}
+}
+
+void DesignExtractor::extractUsesFromCallNodes()
+{
+	ASTNode* node;
+	std::vector<std::string> variables;
+
+	for (int i = 0; i < _callNodes.size(); i++) {
+		node = _callNodes[i];
+		
+		variables = _uses->getUsedVar(node->getName());
+
+		while (isStatement(node)) {
+			for (int j = 0; j < variables.size(); j++) {
+				//std::cout << "Uses(" << node->getStatementNumber() << ", " << variables[j] << ")" << std::endl;
+				_uses->addUsesStmt(node->getStatementNumber(), variables[j]);
+			}
+
+			node = node->getParent()->getParent();
+		}
+	}
+}
 
 bool DesignExtractor::isStatement(ASTNode* node)
 {
@@ -209,11 +287,4 @@ bool DesignExtractor::isUses(ASTNode* node)
 	}
 
 	return false;
-}
-
-void DesignExtractor::validateCall(ASTNode* node)
-{
-	if (_procTable->getIndex(node->getName()) < 0) {
-		throw ParseException(node->getStatementNumber(), node->getName(), "Called procedure does not exist");
-	}
 }
--- Project/source/DesignExtractor.h
+++ Project/source/DesignExtractor.h
@@ -3,6 +3,7 @@
 #include "PKB.h"
 
 #include <vector>
+#include <utility>
 #include <iostream>
 
 class DesignExtractor
@@ -25,11 +26,13 @@ private:
 	Modifies* _modifies;
 	Uses* _uses;
 
+	Calls* _calls;
+
 	int _numOfStmt;
 
-	std::vector<ASTNode*> _calls;
+	std::vector<ASTNode*> _callNodes;
 
-	void traverseAndExtract(ASTNode* node, std::vector<int> statements, std::string procedure, int prevLine);
+	void traverseAST(ASTNode* node, std::vector<int> statements, std::string procedure);
 
 	void updateStatements(ASTNode* node, std::vector<int> &statements);
 	void updateProcedure(ASTNode* node, std::string &procedure);
@@ -42,8 +45,18 @@ private:
 
 	void extractCalls(ASTNode* node, std::string procedure);
 
-	//void extractModifiesFromCallNodes();
-	//void extractUsesFromCallNodes();
+	void validateCall(ASTNode* node);
+
+	void traverseCalls();
+	void traverseCalls(std::string procedure, std::vector<int> &visited, std::vector<std::pair<std::string, std::string>> &calls);
+
+	void extractModifiesFromCall(std::pair<std::string, std::string> call);
+	void extractUsesFromCall(std::pair<std::string, std::string> call);
+
+	void extractFromCallNodes();
+
+	void extractModifiesFromCallNodes();
+	void extractUsesFromCallNodes();
 
 	bool isStatement(ASTNode* node);
 	bool isFirstChild(ASTNode* node);
@@ -52,6 +65,4 @@ private:
 	bool isParent(ASTNode* node);
 	bool isModifies(ASTNode* node);
 	bool isUses(ASTNode* node);
-
-	void validateCall(ASTNode* node);
 };
