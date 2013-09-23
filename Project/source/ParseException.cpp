a9611ce15a84eeaaaec059de8906eec7480d45f5
Jun Yong <junyong.kwa@gmail.com>
2013-09-24 03:25:34 +0800

    Modified ParseException for Parser to work

--- Project/source/ParseException.cpp
+++ Project/source/ParseException.cpp
@@ -1,23 +1,56 @@
 #include "ParseException.h"
 
-ParseException::ParseException(std::string token, std::string message)
-{
-	_message += "Token: \"" + token + "\"\n";
-	_message += "\n";
-	_message += "Message:\n";
-	_message += message;
-}
-
 ParseException::ParseException(int stmtNum, std::string token, std::string message)
 {
-	_message += "Line: " + std::to_string(long double(stmtNum)) + "\n";
-	_message += "Token: \"" + token + "\"\n";
-	_message += "\n";
-	_message += "Message:\n";
-	_message += message;
+	_stmtNum = stmtNum;
+	_token = token;
+	_message = message;
+
+	updateWhat();
+}
+
+ParseException::ParseException(std::string token, std::string message)
+{
+	_stmtNum = 0;
+	_token = token;
+	_message = message;
+
+	updateWhat();
+}
+
+ParseException::ParseException(std::string message)
+{
+	_stmtNum = 0;
+	_token = "";
+	_message = message;
+
+	updateWhat();
+}
+
+void ParseException::setStatementNumber(int stmtNum)
+{
+	_stmtNum = stmtNum;
+
+	updateWhat();
+}
+
+void ParseException::updateWhat()
+{
+	_what = "";
+
+	if (_stmtNum > 0) {
+		_what += "Line: " + std::to_string(long double(_stmtNum)) + "\n\n";
+	}
+	
+	if (_token != "") {
+		_what += "Token: \"" + _token + "\"\n\n";
+	}
+
+	_what += "Message:\n";
+	_what += _message + "\n\n";
 }
 
 const char* ParseException::what() const throw()
 {
-	return _message.c_str();
+	return _what.c_str();
 }
--- Project/source/ParseException.h
+++ Project/source/ParseException.h
@@ -6,11 +6,20 @@
 class ParseException: public std::exception
 {
 public:
-	ParseException(std::string token, std::string message);
 	ParseException(int stmtNum, std::string token, std::string message);
+	ParseException(std::string token, std::string message);
+	ParseException(std::string message);
+
+	void setStatementNumber(int stmtNum);
 
 	const char* what() const throw();
 
 private:
+	int _stmtNum;
+	std::string _token;
 	std::string _message;
+
+	std::string _what;
+
+	void updateWhat();
 };
