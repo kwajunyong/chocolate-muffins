66dd039e58da664c00561256f1f804a92559766a
Hendri <emailhendri@gmail.com>
2013-10-06 22:15:57 +0800

    intermediate commit

--- Project/IntegrationTesting/source/IntegrationTest.cpp
+++ Project/IntegrationTesting/source/IntegrationTest.cpp
@@ -58,5 +58,5 @@ int main(int argc, char* argv[])
 
 	qm->execute();
 	list<string> l = qm->outputResult();
-
+	return 0;
 }
--- Project/source/ExpressionPattern.cpp
+++ Project/source/ExpressionPattern.cpp
@@ -1,22 +1,18 @@
 #include "ExpressionPattern.h"
 #include "QueryManager.h"
-
+#include "BinaryRelationEngine.h"
 ExpressionPattern::ExpressionPattern(QueryManager* qm, PKB *pkb) : QueryClass(QT_MODIFIES, qm, pkb){
 
 
 }
 void ExpressionPattern::run() {
-		QueryClass *exp ;
+		QueryClass *exp =new BinaryRelationEngine(NULL, NULL);
 
-	exp->addParam("a", VT_ASSIGNMENT);
-	exp->addParam("", VT_UNDERSCORE);
-	exp->addParam("x+1", VT_EXPRESSION_UNDERSCORE);
-	// pre check is here
 
-	// if it's procedure and variable combination
-	ASTParameter *astParam1 = parameterList.at(0);
-	ASTParameter *astParam2 = parameterList.at(1);	
+		exp->addParam("a", VT_CALL, "a");
 
+ 
+		
 
 }
 
