86e881ae8f3be43a9341c29138280b082c02f2b6
Jun Yong <junyong.kwa@gmail.com>
2013-10-07 12:50:38 +0800

    Fixed IntegrationTest.cpp

--- Project/IntegrationTesting/source/IntegrationTest.cpp
+++ Project/IntegrationTesting/source/IntegrationTest.cpp
@@ -1,62 +1,26 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include <iostream>
-#include "ListManager.h"
-#include "QueryManager.h"
-#include "Parser.h"
-#include "DesignExtractor.h"
-#include "PKB.h"
-#include "QueryValidator.h"
+
 
 using namespace std;
 
 int main1(int argc, char* argv[])
 {
-	Parser parser;
-	DesignExtractor extractor;
-	PKB *pkb;
-	QueryManager *qm;
-	QueryValidator *qv;
+	// Get the top level suite from the registry
+	CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+	CppUnit::TextUi::TestRunner runner;
 
-	try {
-		pkb = parser.parse("C:\\Study\\CS3202\\simpleTestCase1.txt");
-		extractor.extract(pkb);
-		qm = new QueryManager(pkb);
-		qv = new QueryValidator(qm, pkb);
+	//parse the SIMPLE soource code into the parser
+	//Parse();
 
-	} catch (ParseException pe) {
-		cout << pe.what();
-		getchar();
-	}
-	//Sample 1
-	//variable v;
-//Select v such that modifies(2, v)
-	/*qm->addExpression(VT_VARIABLELIST, "v");
-	qm->addResultExpression("v");
-	QueryClass *qc = new ModifiesEngine(qm, pkb);
-	qc->addParam("2", VT_CONSTANTINTEGER);
-	qc->addParam("v", VT_VARIABLELIST);
+	//Call of DesignExtractor
+	//Extract();
 
-	qm->addQueryClass(qc);
+	runner.addTest(suite);
+	bool wasSucessful = runner.run();
 
-	qm->execute();
-	list<string> l = qm->outputResult();*/
+	getchar();
 
-	// Sample 3
-//	while w; variable v;
-//Select v such that Modifies(w, v)
-
-	qm->addExpression(VT_VARIABLELIST, "v");
-	qm->addExpression(VT_WHILE, "w");
-
-	qm->addResultExpression("v");
-	QueryClass *qc = new ModifiesEngine(qm, pkb);
-	qc->addParam("w", VT_WHILE);
-	qc->addParam("v", VT_VARIABLELIST);
-
-	qm->addQueryClass(qc);
-
-	qm->execute();
-	list<string> l = qm->outputResult();
-	return 0;
+	return wasSucessful ? 0 : 1;
 }
