db3bfcb425196170c5a09a8c8b4e8d1a26801b88
Hendri <emailhendri@gmail.com>
2013-11-05 12:10:02 +0800

    Comment out unnecessary code

--- Project/source/QueryTree.cpp
+++ Project/source/QueryTree.cpp
@@ -61,27 +61,29 @@ void QueryTree::addQueryClass(QueryClass *qc) {
 }
 
 QueryClass *QueryTree::pop() { // pop one now, will pop more than one if they are the same generation. 
-	QueryClass *qc = queryList[0];
+/*	QueryClass *qc = queryList[0];
 
 	queryList.erase(queryList.begin());
 	
 	return qc;
-
+	*/
+	return NULL;
 
 }
 
 bool QueryTree::isEmpty() {
-	return queryList.empty();
+	//return queryList.empty();
+	return false;
 }
 
 void QueryTree::clear() {
 
 	vector<QueryClass*>::iterator iter;
 
-	for (iter = queryList.begin(); iter != queryList.end(); iter++) {
-	    delete (*iter);
+	//for (iter = queryList.begin(); iter != queryList.end(); iter++) {
+	  //  delete (*iter);
 
-	}
+	//}
 
-	queryList.clear();
+	//queryList.clear();
 }
